e1a5c9ad1f683593e405f049c7e505954051a499
maaricse <maaricse8@gmail.com>
2016-04-05 12:19:56 +0530

    Create Numbers.c

---
+++ Numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a=10;
+	scanf("%d",&a);
+	if(a==0)
+	printf("%d is a Zero",a);
+	else if(a<0)
+	printf("%d is a Negative Number",a);
+	else
+	printf("%d is a positive Number",a);
+	
+}
